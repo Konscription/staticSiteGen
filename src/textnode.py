00bbcf4a4aa59ef85921801214663caef96a9b0e
Konscription <konviction@gmail.com>
2024-07-02 19:15:23 -0400

    added split delimiter and tests

--- src/textnode.py
+++ src/textnode.py
@@ -13,6 +13,14 @@
 #Where TEXT, TEXT_TYPE, and URL are the values of the text, text_type, and url properties, respectively.
 from htmlnode import LeafNode
 
+text_type_text = "text"
+text_type_bold = "bold"
+text_type_italic = "italic"
+text_type_code = "code"
+text_type_link = "link"
+text_type_image = "image"
+
+
 class TextNode:
     def __init__(self, text, text_type, url = None):
         self.text = text 
@@ -45,3 +53,24 @@ def text_node_to_html_node(text_node):
     if text_type == "image":
         return LeafNode("img",'',{"src":text_node.url,"alt":text})
     raise ValueError(f"Invalid text type: {text_type}")
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    outputNodes = []
+    for node in old_nodes:
+        if node.text_type != text_type_text:
+            outputNodes.append(node)
+        split_text = node.text.split(delimiter)
+        if len(split_text) %2 == 0:
+            raise ValueError(f"Invalid Markdown Syntax in {node.text}")
+        for x in range(len(split_text)):
+            #print(f"\n{split_text[x]}")
+            if x % 2 == 0:
+                outputNodes.append(TextNode(split_text[x],text_type_text))
+            else:
+                outputNodes.append(TextNode(split_text[x],text_type))
+    return outputNodes
+
+            
+
+
+
