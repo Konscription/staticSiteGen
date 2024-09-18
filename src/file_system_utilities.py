0e321ab59a7e932f927ffafaff25ea0744025185
Konscription <konviction@gmail.com>
2024-09-18 15:32:04 -0400

    create recursive directory clear and copy

---
+++ src/file_system_utilities.py
@@ -0,0 +1,27 @@
+import os
+import shutil
+
+
+def copy_dir(source_dir: str, destination_dir: str):
+    """copy directory tree of a given directory to a given destination directory.
+    does not preserve destination data.
+
+    Args:
+        source_dir (str): source path directory string
+        destination_dir (str): destination path directory string
+    """    
+    if(os.path.exists(destination_dir)):
+        shutil.rmtree(destination_dir)
+    
+    for item in os.listdir(source_dir):
+        source_path = os.path.join(source_dir, item)
+        destination_path = os.path.join(destination_dir, item)
+        
+        if os.path.isfile(source_path):
+            shutil.copy(source_path, destination_path)
+        elif os.path.isdir(source_path):
+            if not os.path.exists(destination_path):
+                os.makedirs(destination_path)
+            copy_dir(source_path, destination_path)
+    
+
--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from textnode import TextNode
-
+from file_system_utilities import copy_dir
 #IMPORT AND USE IT
 #In Python, if you want to use code from one file in another, you need to import it. In main.py, import the TextNode class from textnode.py:
 #
@@ -11,6 +11,10 @@ from textnode import TextNode
 #TextNode(This is a text node, bold, https://www.boot.dev)
 #
 def main():
-    node = TextNode("This is a text node", "bold", "https://www.boot.dev")
-    print(node)
+    #node = TextNode("This is a text node", "bold", "https://www.boot.dev")
+    #print(node)  
+    source = r"./static/"
+    destination = r"./public/"    
+    copy_dir(source, destination)
+
 main()
